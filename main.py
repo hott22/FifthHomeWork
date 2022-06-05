71f1ebfdf703d6eff08e78d5dee0ec337d980ac9
Bobby <mif210210@gmail.com>
2022-06-05 19:48:00 +0300

    edit

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from config import Token
+from config import Token, open_weather_token
+import requests
 
 bot = Bot(token=Token)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
-def math_msg(msg):
-    return eval(msg)
+def get_weather(city, open_weather_token, ru=None):
+    try:
+        r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}'
+                         f'&appid={open_weather_token}&lang={ru}')
+        data = r.json()
+        print(data)
+    except Exception as ex:
+        print(ex)
+        print('Проверьте название города')
+
+
+
+
+def main():
+    city = input('Введите город: ')
+    get_weather(city, open_weather_token)
+
+
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
@@ -18,12 +35,11 @@ async def send_welcome(message: types.Message):
     await message.reply("/math - математические примеры\n")
 
 @dp.message_handler(commands=["math"])
-async def cmd_test1(message: types.Message):
+async def math(message: types.Message):
     await message.reply("Введи математический пример:")
     @dp.message_handler()
-    async def echo(message: types.Message):
-        await message.answer(message.text)
-        await message.reply(math_msg(message.text))
+    async def math2(message: types.Message):
+        await message.reply(eval(message.text))
 
 
 if __name__ == "__main__":
